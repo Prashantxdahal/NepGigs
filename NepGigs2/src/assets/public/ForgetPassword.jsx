297e2691948ba22d87fe470ed7eeccbe5fa41898
Samikshya Paudel <samikshyapaudel61@gmail.com>
2025-06-11 15:41:30 +0545

    added functionality of forget password

--- NepGigs2/src/assets/public/ForgetPassword.jsx
+++ NepGigs2/src/assets/public/ForgetPassword.jsx
@@ -13,21 +13,24 @@ const ForgetPassword = () => {
     number: /[0-9]/.test(password),
   };
 
-
+  
   return (
     <div className="reset-container">
-      <div className="reset-left">
-        <img src={resetImage} alt="Reset Password" />
-      </div>
-      <div className="reset-right">
-        <h2>Reset Password</h2>
-        <label>New Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        
+      
+        <ul className="password-rules">
+          <li className={passwordChecks.length ? "valid" : ""}>
+            {passwordChecks.length ? "✅" : "⭕"} At least 8 characters
+          </li>
+          <li className={passwordChecks.uppercase ? "valid" : ""}>
+            {passwordChecks.uppercase ? "✅" : "⭕"} At least 1 uppercase letter
+          </li>
+          <li className={passwordChecks.lowercase ? "valid" : ""}>
+            {passwordChecks.lowercase ? "✅" : "⭕"} At least 1 lowercase letter
+          </li>
+          <li className={passwordChecks.number ? "valid" : ""}>
+            {passwordChecks.number ? "✅" : "⭕"} At least 1 number
+          </li>
+        </ul>
 
         <label>Confirm Password</label>
         <input

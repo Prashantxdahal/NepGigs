1fd31041618aa702275800f0f9189251b5926a05
purnakc <166592833+purnakc@users.noreply.github.com>
2025-07-19 16:51:21 +0900

    admimdashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HowItWorks from "./core/Public/HowItWorks";
 import UserDashboard from "./core/Public/UserDashboard";
 import GigsPosting from "./core/Public/GigsPosting";
 import BuyerDashboard from "./core/Public/BuyerDashboard";
+import AdminDashboard from "./core/Public/AdminDashboard";
 function App() {
   return (
     <Routes>
@@ -13,6 +14,7 @@ function App() {
       <Route path="/HowItWorks" element={<HowItWorks />} />
       <Route path="/GigsPosting" element={<GigsPosting />} />
       <Route path="/BuyerDashboard" element={<BuyerDashboard />} />
+      <Route path="/AdminDashboard" element={<AdminDashboard />} />
       <Route path="/register" element={<Register />} />
     </Routes>
   );
